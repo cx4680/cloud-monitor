a50af20aa68ce9484ef33f68d2a86ddce93d614e
wu.juan <wu.juan@cestc.cn>
2021-11-08 11:09:29 +0800

    update: 定时任务异步开启

--- cloud-monitor-center/main.go
+++ cloud-monitor-center/main.go
@@ -18,7 +18,7 @@ import (
 // @BasePath /
 func main() {
 	//解析命令参数
-	var cf = flag.String("config.yml", "D:\\dev-go\\cloud-monitor\\cloud-monitor-center\\config.yml.local.yml", "config.yml path")
+	var cf = flag.String("config.yml", "config.local.yml", "config.yml path")
 	flag.Parse()
 
 	//加载配置文件
