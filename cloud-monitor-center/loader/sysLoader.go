72ebd91732119b07492da96961adbfa715f7f237
谢正龙 <xiezhenglong@cestc.cn>
2021-11-30 16:27:50 +0800

    update:优化代码，提取main方法的模板

---
+++ cloud-monitor-center/loader/sysLoader.go
@@ -0,0 +1,51 @@
+package loader
+
+import (
+	"code.cestc.cn/ccos-ops/cloud-monitor/business-common/global/loader"
+	"code.cestc.cn/ccos-ops/cloud-monitor/business-common/global/sysComponent/sysRocketMq"
+	"code.cestc.cn/ccos-ops/cloud-monitor/business-common/task"
+	"code.cestc.cn/ccos-ops/cloud-monitor/cloud-monitor-center/mq/consumer"
+	"code.cestc.cn/ccos-ops/cloud-monitor/cloud-monitor-center/validator/translate"
+	"code.cestc.cn/ccos-ops/cloud-monitor/cloud-monitor-center/web"
+	"code.cestc.cn/ccos-ops/cloud-monitor/common/config"
+)
+
+type SysLoaderImpl struct {
+	loader.AbstractSysLoader
+}
+
+func (s SysLoaderImpl) InitTask() error {
+	bt := task.NewBusinessTaskImpl()
+	if err := bt.Add(task.BusinessTaskDTO{
+		Cron: "0 0 0/1 * * ?",
+		Name: "clearAlertRecordJob",
+		Task: task.Clear,
+	}); err != nil {
+		return err
+	}
+
+	bt.Start()
+	return nil
+}
+
+func (s SysLoaderImpl) InitRocketMqConsumers() error {
+	//TODO 初始化消费者
+	if err := sysRocketMq.StartConsumersScribe("cloud-monitor-center", []*sysRocketMq.Consumer{{
+		Topic:   sysRocketMq.InstanceTopic,
+		Handler: consumer.InstanceHandler,
+	}}); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s SysLoaderImpl) InitWebServe() error {
+	if err := web.Start(config.GetServeConfig()); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s SysLoaderImpl) InitTrans() error {
+	return translate.InitTrans("zh")
+}
--- cloud-monitor-region/main.go
+++ cloud-monitor-region/main.go
@@ -1,107 +1,14 @@
 package main
 
 import (
-	"code.cestc.cn/ccos-ops/cloud-monitor/business-common/global/sysComponent"
-	"code.cestc.cn/ccos-ops/cloud-monitor/business-common/global/sysComponent/sysRocketMq"
-	commonTask "code.cestc.cn/ccos-ops/cloud-monitor/business-common/task"
-	"code.cestc.cn/ccos-ops/cloud-monitor/cloud-monitor-region/k8s"
-	"code.cestc.cn/ccos-ops/cloud-monitor/cloud-monitor-region/mq/consumer"
-	"code.cestc.cn/ccos-ops/cloud-monitor/cloud-monitor-region/task"
-	"code.cestc.cn/ccos-ops/cloud-monitor/cloud-monitor-region/validator/translate"
-	"code.cestc.cn/ccos-ops/cloud-monitor/cloud-monitor-region/web"
-	"code.cestc.cn/ccos-ops/cloud-monitor/common/config"
-	"code.cestc.cn/ccos-ops/cloud-monitor/common/logger"
-	"flag"
-	"log"
+	"code.cestc.cn/ccos-ops/cloud-monitor/cloud-monitor-region/loader"
 	"os"
 )
 
 func main() {
-	//解析命令参数
-	var cf = flag.String("config", "config.local.yml", "config.yml path")
-	flag.Parse()
-
-	//加载配置文件
-	if err := config.InitConfig(*cf); err != nil {
-		log.Printf("init config.yml error: %v\n", err)
-		os.Exit(1)
+	l := &loader.SysLoaderImpl{}
+	if err := l.StartServe(l); err != nil {
+		os.Exit(-1)
 	}
 
-	if err := translate.InitTrans("zh"); err != nil {
-		log.Printf("init trans failed, err:%v\n", err)
-		os.Exit(2)
-	}
-
-	if err := sysComponent.InitSys(); err != nil {
-		log.Printf("init sys component error: %v\n", err)
-		os.Exit(3)
-	}
-
-	if config.GetCommonConfig().Env != "local" {
-		if err := k8s.InitK8s(); err != nil {
-			log.Printf("init k8s error: %v\n", err)
-			os.Exit(5)
-		}
-	}
-	//加载mq
-	if err := initRocketMqConsumers(); err != nil {
-		log.Printf("init rocketmq error, %v\n", err)
-		os.Exit(5)
-	}
-	//加载定时任务
-	if err := initTask(); err != nil {
-		log.Printf("init task error, %v\n", err)
-		os.Exit(6)
-	}
-
-	logger.InitLogger(config.GetLogConfig())
-	defer logger.Logger().Sync()
-
-	//启动Web容器
-	err := web.Start(config.GetServeConfig())
-	if err != nil {
-		logger.Logger().Infof("startup service failed, err:%v\n", err)
-		os.Exit(6)
-	}
-}
-
-func initRocketMqConsumers() error {
-	if err := sysRocketMq.StartConsumersScribe(sysRocketMq.Group(config.GetCommonConfig().RegionName), []*sysRocketMq.Consumer{{
-		Topic:   sysRocketMq.AlertContactTopic,
-		Handler: consumer.AlertContactHandler,
-	}, {
-		Topic:   sysRocketMq.RuleTopic,
-		Handler: consumer.AlarmRuleHandler,
-	}}); err != nil {
-		log.Printf("create rocketmq consumer error, %v\n", err)
-		return err
-	}
-	return nil
-}
-
-func initTask() error {
-	bt := commonTask.NewBusinessTaskImpl()
-	var corn = "0 0 0/1 * * ?"
-	if err := bt.Add(commonTask.BusinessTaskDTO{
-		Cron: corn,
-		Name: "instanceJob",
-		Task: task.NewEcsJob().SyncJob,
-	}); err != nil {
-		return err
-	}
-
-	if err := bt.Add(commonTask.BusinessTaskDTO{
-		Cron: corn,
-		Task: task.NewSlbJob().SyncJob,
-	}); err != nil {
-		return err
-	}
-	if err := bt.Add(commonTask.BusinessTaskDTO{
-		Cron: corn,
-		Task: task.NewEipJob().SyncJob,
-	}); err != nil {
-		return err
-	}
-	bt.Start()
-	return nil
 }
