001c0bc2f49cbeef289d4a9103f2264c88b99989
wu.juan <wu.juan@cestc.cn>
2021-12-07 14:51:24 +0800

    add:   iam

--- cloud-monitor-region/web/router.go
+++ cloud-monitor-region/web/router.go
@@ -1,7 +1,7 @@
 package web
 
 import (
-	commonDao "code.cestc.cn/ccos-ops/cloud-monitor/business-common/dao"
+	"code.cestc.cn/ccos-ops/cloud-monitor/business-common/global/iam"
 	commonService "code.cestc.cn/ccos-ops/cloud-monitor/business-common/service"
 	"code.cestc.cn/ccos-ops/cloud-monitor/business-common/service/external/messageCenter"
 	"code.cestc.cn/ccos-ops/cloud-monitor/cloud-monitor-region/actuator"
@@ -10,6 +10,7 @@ import (
 	"code.cestc.cn/ccos-ops/cloud-monitor/cloud-monitor-region/docs"
 	"code.cestc.cn/ccos-ops/cloud-monitor/cloud-monitor-region/inner"
 	"code.cestc.cn/ccos-ops/cloud-monitor/cloud-monitor-region/service"
+	"code.cestc.cn/yyptb-group_tech/iam-sdk-go/pkg/models"
 	"github.com/gin-gonic/gin"
 	gs "github.com/swaggo/gin-swagger"
 	"github.com/swaggo/gin-swagger/swaggerFiles"
@@ -17,32 +18,24 @@ import (
 )
 
 func loadRouters() {
-	monitorProductRouters()
 	swagger()
 	instance()
 	eip()
 	slb()
 	nat()
+	cbr()
 	MonitorReportForm()
 	innerCtl()
+	actuatorMapping()
 }
 
 func MonitorReportForm() {
 	monitorReportFormCtl := controllers.NewMonitorReportFormController(service.NewMonitorReportFormService())
 	group := router.Group("/hawkeye/MonitorReportForm/")
 	{
-		group.GET("/getData", monitorReportFormCtl.GetData)
-		group.GET("/getAxisData", monitorReportFormCtl.GetAxisData)
-		group.GET("/getTop", monitorReportFormCtl.GetTop)
-	}
-}
-
-func monitorProductRouters() {
-	monitorProductCtl := controllers.NewMonitorProductCtl(commonDao.MonitorProduct)
-	group := router.Group("/hawkeye/monitorProduct/")
-	{
-		group.GET("/getById", monitorProductCtl.GetById)
-		group.PUT("/updateById", monitorProductCtl.UpdateById)
+		group.GET("/getData", iam.AuthIdentify(&models.Identity{Product: iam.ProductMonitor, Action: "GetMonitorReportData", ResourceType: "*", ResourceId: "*"}), monitorReportFormCtl.GetData)
+		group.GET("/getAxisData", iam.AuthIdentify(&models.Identity{Product: iam.ProductMonitor, Action: "GetMonitorReportRangeData", ResourceType: "*", ResourceId: "*"}), monitorReportFormCtl.GetAxisData)
+		group.GET("/getTop", iam.AuthIdentify(&models.Identity{Product: iam.ProductMonitor, Action: "GetMonitorReportTop", ResourceType: "*", ResourceId: "*"}), monitorReportFormCtl.GetTop)
 	}
 }
 
@@ -55,8 +48,8 @@ func instance() {
 	instanceCtl := controllers.NewInstanceCtl(dao.Instance)
 	group := router.Group("/hawkeye/instance/")
 	{
-		group.GET("/page", instanceCtl.Page)
-		group.GET("/getInstanceNum", instanceCtl.GetInstanceNumByRegion)
+		group.GET("/page", iam.AuthIdentify(&models.Identity{Product: iam.ProductMonitor, Action: "GetInstancePageList", ResourceType: "*", ResourceId: "*"}), instanceCtl.Page)
+		group.GET("/getInstanceNum", iam.AuthIdentify(&models.Identity{Product: iam.ProductMonitor, Action: "GetInstanceNum", ResourceType: "*", ResourceId: "*"}), instanceCtl.GetInstanceNumByRegion)
 	}
 }
 
@@ -64,7 +57,7 @@ func eip() {
 	ctl := controllers.NewEipCtl()
 	group := router.Group("/hawkeye/eip/")
 	{
-		group.GET("/page", ctl.Page)
+		group.GET("/page", iam.AuthIdentify(&models.Identity{Product: iam.ProductMonitor, Action: "GetEipPageList", ResourceType: "*", ResourceId: "*"}), ctl.Page)
 	}
 }
 
@@ -72,7 +65,7 @@ func slb() {
 	ctl := controllers.NewSlbCtl()
 	group := router.Group("/hawkeye/slb/")
 	{
-		group.GET("/page", ctl.Page)
+		group.GET("/page", iam.AuthIdentify(&models.Identity{Product: iam.ProductMonitor, Action: "GetSlbPageList", ResourceType: "*", ResourceId: "*"}), ctl.Page)
 	}
 }
 
@@ -80,7 +73,7 @@ func cbr() {
 	ctl := controllers.NewCbrCtl()
 	group := router.Group("/hawkeye/cbr/")
 	{
-		group.GET("/page", ctl.Page)
+		group.GET("/page", iam.AuthIdentify(&models.Identity{Product: iam.ProductMonitor, Action: "GetCbrPageList", ResourceType: "*", ResourceId: "*"}), ctl.Page)
 	}
 }
 
@@ -88,7 +81,7 @@ func nat() {
 	ctl := controllers.NewNatCtl()
 	group := router.Group("/hawkeye/nat/")
 	{
-		group.GET("/page", ctl.Page)
+		group.GET("/page", iam.AuthIdentify(&models.Identity{Product: iam.ProductMonitor, Action: "GetNatPageList", ResourceType: "*", ResourceId: "*"}), ctl.Page)
 	}
 }
 
