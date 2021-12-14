57fa24fcd5db16834c1c451ef29c65d710a603e7
陈行 <chenxing@cestc.cn>
2021-12-14 09:59:14 +0800

    update：补充监控产品信息

--- cloud-monitor-region/constant/MonitorReportFormConstans.go
+++ cloud-monitor-region/constant/MonitorReportFormConstans.go
@@ -6,4 +6,11 @@ const (
 	MetricLabel        = "$INSTANCE"
 	EcsCpuUsage        = "ecs_cpu_usage"
 	EcsCpuUsageTopExpr = "topk(5,(100 - (100 * (sum by(instance) (irate(ecs_cpu_seconds_total{mode='idle',$INSTANCE}[3m])) / sum by(instance) (irate(ecs_cpu_seconds_total{$INSTANCE}[3m]))))))"
+
+	EcsProduct   = "1"
+	EipProduct   = "2"
+	SlbProduct   = "3"
+	MySqlProduct = "4"
+	CbrProduct   = "5"
+	NatProduct   = "6"
 )
