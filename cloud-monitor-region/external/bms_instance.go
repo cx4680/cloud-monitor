146bed4e47edfb357e370d74a6aaf4ac6374430f
陈行 <chenxing@cestc.cn>
2022-01-26 15:45:44 +0800

    update:bms

--- cloud-monitor-region/external/bms_instance.go
+++ cloud-monitor-region/external/bms_instance.go
@@ -13,19 +13,11 @@ type BmsInstanceService struct {
 	service.InstanceServiceImpl
 }
 
-type BmsQueryPageRequest struct {
-	PageNumber       int    `json:"pageNumber,omitempty"`
-	PageSize         int    `json:"pageSize,omitempty"`
-	TenantId         string `json:"tenantId,omitempty"`
-	AllTenants       string `json:"all_tenants,omitempty"`
-	Id               string `json:"id,omitempty"`
-	AvailabilityZone string `json:"availability_zone,omitempty"`
-}
-
 type BmsResponse struct {
-	Code    string             `json:"code"`
-	Message string             `json:"message"`
-	Data    BmsQueryPageResult `json:"data"`
+	RequestId string             `json:"RequestId"`
+	Code      string             `json:"code"`
+	Message   string             `json:"message"`
+	Data      BmsQueryPageResult `json:"data"`
 }
 
 type BmsQueryPageResult struct {
@@ -34,50 +26,9 @@ type BmsQueryPageResult struct {
 }
 
 type BmsServers struct {
-	FlavorId string `json:"flavor_id"`
-	Name     string `json:"name"`
-	Status   string `json:"status"`
-	Id       string `json:"id"`
-}
-
-type SerialResponse struct {
-	Code    string                `json:"code"`
-	Message string                `json:"message"`
-	Data    SerialQueryPageResult `json:"data"`
-}
-
-type SerialQueryPageResult struct {
-	Bmcnodes   []Bmcnodes `json:"bmcnodes"`
-	TotalCount int        `json:"total_count"`
-}
-
-type Bmcnodes struct {
-	SerialNumber      string `json:"serial_number"`
-	EcsId             string `json:"ecs_id"`
-	Manufactory       string `json:"manufactory"`
-	Productor         string `json:"productor"`
-	Cpu               int    `json:"cpu"`
-	CpuManufactory    string `json:"cpu_manufactory"`
-	CpuProductor      string `json:"cpu_productor"`
-	CpuArch           string `json:"cpu_arch"`
-	Memory            int    `json:"memory"`
-	SystemDisk        int    `json:"system_disk"`
-	DataDisk          int    `json:"data_disk"`
-	Nic               string `json:"nic"`
-	GpuType           string `json:"gpu_type"`
-	FirmwareVersion   string `json:"firmware_version"`
-	IdcRoom           string `json:"idc_room"`
-	CabinetNum        string `json:"cabinet_num"`
-	RankNum           string `json:"rank_num"`
-	AvailabilityZone  string `json:"availability_zone"`
-	IpmiBmcIp         string `json:"ipmi_bmc_ip"`
-	IpmiBmcUsername   string `json:"ipmi_bmc_username"`
-	IpmiBmcPassword   string `json:"ipmi_bmc_password"`
-	SmartnicManagerIp string `json:"smartnic_manager_ip"`
-	Smartniciqn       string `json:"smartniciqn"`
-	HealthyStatus     bool   `json:"healthy_status"`
-	UnhealthyReason   string `json:"unhealthy_reason"`
-	UserSystemStatus  string `json:"user_system_status"`
+	InstanceId string `json:"InstanceId"`
+	Name       string `json:"name"`
+	State      string `json:"State"`
 }
 
 func (bms *BmsInstanceService) ConvertRealForm(form service.InstancePageForm) interface{} {
@@ -116,11 +67,11 @@ func (bms *BmsInstanceService) ConvertResp(realResp interface{}) (int, []service
 	if vo.Data.TotalCount > 0 {
 		for _, d := range vo.Data.Servers {
 			list = append(list, service.InstanceCommonVO{
-				InstanceId:   d.Id,
+				InstanceId:   d.InstanceId,
 				InstanceName: d.Name,
 				Labels: []service.InstanceLabel{{
 					Name:  "status",
-					Value: d.Status,
+					Value: d.State,
 				}},
 			})
 		}
