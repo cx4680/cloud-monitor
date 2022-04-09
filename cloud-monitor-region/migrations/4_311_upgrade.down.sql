233cad8f9c98fb56dec4b5d8142c1d0f6b082b21
陈行 <chenxing@cestc.cn>
2022-04-09 10:18:02 +0800

    传统裸金属和弹性裸金属区分开

--- cloud-monitor-region/migrations/4_311_upgrade.down.sql
+++ cloud-monitor-region/migrations/4_311_upgrade.down.sql
@@ -4,7 +4,7 @@ UPDATE t_monitor_product SET status = '0' WHERE abbreviation IN ('bms','kafka','
 
 UPDATE t_monitor_item SET type = null WHERE biz_id IN ('68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85');
 
-DELETE FROM t_monitor_item WHERE biz_id in ('166');
+DELETE FROM t_monitor_item WHERE biz_id IN ('166','167','168','169','170','171','172','173','174','175','176','177','178','179','180','181','182','183','184');
 
 DELETE FROM t_monitor_product WHERE abbreviation IN ('bms','ebms');
 INSERT INTO t_monitor_product (biz_id, name, status, description, create_user, create_time, route, cron, host, page_url, abbreviation) VALUES ('7', '裸金属服务器', '0', 'bms', null, null, '/productmonitoring/bms', '0 0 0/1 * * ?', 'http://bms-manage.product-bms:8080', '/compute/bms/ops/v1', 'bms');
