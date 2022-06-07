1dfc614bbdd2e0773ba15e8995ee2ef7212ad31d
陈行 <chenxing@cestc.cn>
2022-06-07 17:05:27 +0800

    update：裸金属能耗监控

--- cmd/migrations/6_313_upgrade.down.sql
+++ cmd/migrations/6_313_upgrade.down.sql
@@ -1,4 +1,4 @@
-DELETE FROM t_monitor_item WHERE biz_id IN ('149','150','151','152','153','154','220','221','222','223','224','225');
+DELETE FROM t_monitor_item WHERE biz_id IN ('149','150','151','152','153','154','220','221','222','223','224','225','226','227','228');
 
 UPDATE t_monitor_product SET status = '0' WHERE abbreviation = 'cgw';
 
