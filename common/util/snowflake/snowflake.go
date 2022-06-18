311def39779d07b1d726a73cf08b5c68c9956e59
xiezhenglong <xiezhenglong@cestc.cn>
2022-06-18 13:58:07 +0800

    update:修改雪花算法生成Id配置，WorkerId改为由podName生成。

--- common/util/snowflake/snowflake.go
+++ common/util/snowflake/snowflake.go
@@ -149,7 +149,7 @@ func GetWorker() *SnowflakeIdWorker {
 
 func getWorkerId(podName string) int64 {
 	if podName != "" {
-		return hashcode(podName) % 31
+		return hashcode(podName) % maxWorkerId
 	}
 	return 0
 }
