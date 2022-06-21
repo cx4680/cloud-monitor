33e4af884475e4636385483cd40ec47b3e6270e3
xiezhenglong <xiezhenglong@cestc.cn>
2022-06-21 14:35:24 +0800

    update: 修改告警模板查询逻辑

--- pkg/api/openapi/v1.0/alarm_rule_template.go
+++ pkg/api/openapi/v1.0/alarm_rule_template.go
@@ -62,15 +62,15 @@ func (ctl *AlarmRuleTemplateCtl) GetRuleListByProduct(c *gin.Context) {
 
 	ruleList := dao.AlarmRuleTemplate.QueryRuleTemplateListByProduct(global.DB, tenantId, productBizId)
 	for i, v := range ruleList {
-		ruleDetail, _ := dao.AlarmRule.GetDetail(global.DB, v.RuleId, tenantId)
-		if ruleDetail != (&form.AlarmRuleDetailDTO{}) {
+		if v.Type == 1 {
+			ruleDetail, _ := dao.AlarmRule.GetDetail(global.DB, v.RuleId, tenantId)
 			cs := make([]string, len(ruleDetail.RuleConditions))
 			for j, condition := range ruleDetail.RuleConditions {
 				cs[j] = dao.GetExpress(*condition)
 			}
 			ruleList[i].RuleName = ruleDetail.RuleName
 			ruleList[i].Conditions = cs
-		} else {
+		} else if v.Type == 0 {
 			itemList := dao.AlarmItemTemplate.QueryItemListByTemplate(global.DB, v.RuleTemplateId)
 			cs := make([]string, len(itemList))
 			for j, item := range itemList {
