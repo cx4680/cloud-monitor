921eb16c061f263f0a06b34f197dc569fa03d9a1
陈行 <chenxing@cestc.cn>
2022-06-23 16:16:39 +0800

    update:修复创建告警规则时，请求体过大造成k8s写入失败的问题
    (cherry picked from commit e24dabbc929c29c9314fc754b4aca70ab2a0782d)

--- pkg/k8s/prometheus_rule.go
+++ pkg/k8s/prometheus_rule.go
@@ -3,11 +3,11 @@ package k8s
 import "code.cestc.cn/ccos-ops/cloud-monitor/pkg/business-common/model"
 
 type AlertRuleDTO struct {
-	AlertRuleId    string        `json:"alertRuleId"`
-	TenantId       string        `json:"tenantId"`
-	Region         string        `json:"region"`         //所属区域
-	Zone           string        `json:"zone"`           // 可用区
-	SpecGroupsList []*SpecGroups `json:"specGroupsList"` //告警规则详情
+	AlertRuleId    string       `json:"alertRuleId"`
+	TenantId       string       `json:"tenantId"`
+	Region         string       `json:"region"`         //所属区域
+	Zone           string       `json:"zone"`           // 可用区
+	SpecGroupsList []SpecGroups `json:"specGroupsList"` //告警规则详情
 }
 
 type SpecGroups struct {
