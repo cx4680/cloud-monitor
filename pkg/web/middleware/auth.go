3c6a64bb5c3565abd3e950db1639fb02cbceaf78
xiezhenglong <xiezhenglong@cestc.cn>
2022-07-04 15:36:40 +0800

    update:添加查询Region下产品实例列表的内部接口

--- pkg/web/middleware/auth.go
+++ pkg/web/middleware/auth.go
@@ -23,6 +23,7 @@ var ignoreList = []string{"/hawkeye/contact/activateContact?*", "/inner/alarmRec
 	"/hawkeye/inner/notice/*",
 	"/hawkeye/inner/monitorChart/*",
 	"/hawkeye/inner/reportForm/*",
+	"/hawkeye/inner/monitorResource/*",
 }
 
 func Auth() gin.HandlerFunc {
