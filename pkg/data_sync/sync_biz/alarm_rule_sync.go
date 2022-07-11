3701780ae6be8cc7d1d5b6145f0cfff6b2f5b502
陈行 <chenxing@cestc.cn>
2022-07-11 17:34:13 +0800

    update：region同步-业务结构修改

--- pkg/data_sync/sync_biz/alarm_rule_sync.go
+++ pkg/data_sync/sync_biz/alarm_rule_sync.go
@@ -2,7 +2,7 @@ package sync_biz
 
 import (
 	"code.cestc.cn/ccos-ops/cloud-monitor/pkg/data_sync"
-	"log"
+	"code.cestc.cn/ccos-ops/cloud-monitor/pkg/service"
 	"time"
 )
 
@@ -20,10 +20,10 @@ func NewAlarmRuleSynchronizer() data_sync.SyncTask {
 	return s
 }
 
-func (ars *AlarmRuleSynchronizer) Work(lastUpdateTime string) error {
-	log.Println("告警规则同步...")
+func (ars *AlarmRuleSynchronizer) Work(lastUpdateTime string) (string, error) {
+	currentTime, err := service.NewRegionSyncService().AlarmRuleSync(lastUpdateTime)
 	time.Sleep(10 * time.Second)
-	return nil
+	return currentTime, err
 }
 
 func (ars *AlarmRuleSynchronizer) Period() time.Duration {
--- pkg/sync/sync.go
+++ pkg/sync/sync.go
@@ -1,83 +1,84 @@
 package sync
 
-import (
-	"code.cestc.cn/ccos-ops/cloud-monitor/common/logger"
-	"code.cestc.cn/ccos-ops/cloud-monitor/pkg/service"
-	"strings"
-	"time"
-)
-
-//const defaultEmptyNameSrv = "${rocketMQEndpoint}"
-
-func InitSync(regionRole string) error {
-	if strings.EqualFold(regionRole, "integration") {
-		logger.Logger().Info("current run mode: single region")
-		return nil
-	}
-	if strings.EqualFold(regionRole, "manager") {
-		logger.Logger().Info("current run mode: multiple regions - manager")
-		return nil
-	}
-	logger.Logger().Info("current run mode: multiple regions - region")
-	contact := AlarmContact{}
-	execute(contact)
-	//rule := AlarmRule{}
-	//execute(rule)
-	//history := AlarmHistory{}
-	//execute(history)
-	return nil
-}
-
-type SyncCornTasker interface {
-	SyncExecute()
-}
-
-type AlarmContact struct{}
-
-type AlarmRule struct{}
-
-type AlarmHistory struct{}
-
-func (c AlarmContact) SyncExecute() {
-	err := service.NewRegionSyncService().ContactSync()
-	if err != nil {
-		logger.Logger().Errorf("同步失败：%v", err)
-	}
-}
-
-func (r AlarmRule) SyncExecute() {
-	err := service.NewRegionSyncService().AlarmRuleSync()
-	if err != nil {
-		logger.Logger().Errorf("同步失败：%v", err)
-	}
-}
-
-func (h AlarmHistory) SyncExecute() {
-	err := service.NewRegionSyncService().AlarmRecordSync()
-	if err != nil {
-		logger.Logger().Errorf("同步失败：%v", err)
-	}
-}
-
-func execute(arg SyncCornTasker) {
-	loop := make(chan int, 1)
-	sync := func() {
-		defer func() {
-			if err := recover(); err != nil {
-				logger.Logger().Info("同步数据：失败原因：v%\n", err)
-			}
-		}()
-		arg.SyncExecute()
-		time.Sleep(30 * time.Second)
-		loop <- 1
-	}
-	loop <- 1
-	go func() {
-		for {
-			select {
-			case <-loop:
-				sync()
-			}
-		}
-	}()
-}
+//
+//import (
+//	"code.cestc.cn/ccos-ops/cloud-monitor/common/logger"
+//	"code.cestc.cn/ccos-ops/cloud-monitor/pkg/service"
+//	"strings"
+//	"time"
+//)
+//
+////const defaultEmptyNameSrv = "${rocketMQEndpoint}"
+//
+//func InitSync(regionRole string) error {
+//	if strings.EqualFold(regionRole, "integration") {
+//		logger.Logger().Info("current run mode: single region")
+//		return nil
+//	}
+//	if strings.EqualFold(regionRole, "manager") {
+//		logger.Logger().Info("current run mode: multiple regions - manager")
+//		return nil
+//	}
+//	logger.Logger().Info("current run mode: multiple regions - region")
+//	contact := AlarmContact{}
+//	execute(contact)
+//	//rule := AlarmRule{}
+//	//execute(rule)
+//	//history := AlarmHistory{}
+//	//execute(history)
+//	return nil
+//}
+//
+//type SyncCornTasker interface {
+//	SyncExecute()
+//}
+//
+//type AlarmContact struct{}
+//
+//type AlarmRule struct{}
+//
+//type AlarmHistory struct{}
+//
+//func (c AlarmContact) SyncExecute() {
+//	err := service.NewRegionSyncService().ContactSync()
+//	if err != nil {
+//		logger.Logger().Errorf("同步失败：%v", err)
+//	}
+//}
+//
+//func (r AlarmRule) SyncExecute() {
+//	err := service.NewRegionSyncService().AlarmRuleSync()
+//	if err != nil {
+//		logger.Logger().Errorf("同步失败：%v", err)
+//	}
+//}
+//
+//func (h AlarmHistory) SyncExecute() {
+//	err := service.NewRegionSyncService().AlarmRecordSync()
+//	if err != nil {
+//		logger.Logger().Errorf("同步失败：%v", err)
+//	}
+//}
+//
+//func execute(arg SyncCornTasker) {
+//	loop := make(chan int, 1)
+//	sync := func() {
+//		defer func() {
+//			if err := recover(); err != nil {
+//				logger.Logger().Info("同步数据：失败原因：v%\n", err)
+//			}
+//		}()
+//		arg.SyncExecute()
+//		time.Sleep(30 * time.Second)
+//		loop <- 1
+//	}
+//	loop <- 1
+//	go func() {
+//		for {
+//			select {
+//			case <-loop:
+//				sync()
+//			}
+//		}
+//	}()
+//}
