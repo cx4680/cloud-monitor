9c1cf97e721c00b77776869b0748d7721369790a
xiezhenglong <xiezhenglong@cestc.cn>
2022-07-12 14:20:08 +0800

    update:修改同步器启动逻辑

--- pkg/data_sync/sync_init/sync_init.go
+++ pkg/data_sync/sync_init/sync_init.go
@@ -29,7 +29,7 @@ func InitSync(regionRole string) error {
 	go run_time.SafeRun(func() {
 		startHeartBeatCheckTask()
 	})
-	return startSync()
+	return startSyncTask()
 
 }
 
@@ -45,7 +45,7 @@ func startHeartBeatCheckTask() {
 			}
 			if len(val) == 0 {
 				logger.Logger().Info("sync task is null, start running sync task")
-				err := startSync()
+				err := startSyncTask()
 				if err != nil {
 					logger.Logger().Info("start sync task error, %v", err)
 				}
@@ -54,12 +54,16 @@ func startHeartBeatCheckTask() {
 	}
 }
 
-func startSync() error {
+func startSyncTask() error {
 	ctx := context.Background()
 	lockKey := constant.SyncStartKey
+	var started = make(chan int)
 
 	defer func() {
+		//休眠1s，确保已成功发送心跳
+		<-started
 		err := sys_redis.Unlock(ctx, lockKey)
+		logger.Logger().Info("release sync lock")
 		if err != nil {
 			logger.Logger().Errorf("unlock errorL%+v, lock:%s", err, lockKey)
 		}
@@ -70,6 +74,7 @@ func startSync() error {
 		log.Printf("获取 rule lock error  %+v", err)
 		return err
 	}
+	logger.Logger().Info("get SyncStartKey lock")
 	val, err := sys_redis.Get(constant.SyncFlagKey)
 	if err != nil && err != redis.Nil {
 		logger.Logger().Errorf("get sync flag key error, %v", err)
@@ -77,9 +82,10 @@ func startSync() error {
 	}
 	if len(val) > 0 {
 		logger.Logger().Info("already start sync task, not need running ")
+		started <- 1
 		return nil
 	}
-
+	logger.Logger().Info("sync task not running, start running...")
 	synchronizer, err := data_sync.NewCombinedSynchronizer([]data_sync.SyncTask{sync_biz.NewContactSynchronizer(), sync_biz.NewAlarmRuleSynchronizer(), sync_biz.NewAlarmRecordSynchronizer()})
 	if err != nil {
 		return err
@@ -88,24 +94,29 @@ func startSync() error {
 	if err != nil {
 		return err
 	}
-
 	heartBeatTask()
+	started <- 1
 	logger.Logger().Info("sync task is running success!")
 	return nil
 }
 
 func heartBeatTask() {
+	heartBeatFlag := func() {
+		err := sys_redis.SetByTimeOut(constant.SyncFlagKey, "started", 30*time.Second)
+		if err != nil {
+			logger.Logger().Info("set started flag error, %v", err)
+		}
+	}
+	heartBeatFlag()
 	//同步器心跳
 	go run_time.SafeRun(func() {
 		logger.Logger().Info("start sync heartbeat... ")
 		ticker := time.NewTicker(10 * time.Second)
+		defer ticker.Stop()
 		for {
 			select {
 			case <-ticker.C:
-				err := sys_redis.SetByTimeOut(constant.SyncFlagKey, "started", 30*time.Second)
-				if err != nil {
-					logger.Logger().Info("set started flag error, %v", err)
-				}
+				heartBeatFlag()
 			}
 		}
 	})
