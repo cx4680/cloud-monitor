b456b754422270f3fdb5257a982e46d5e10f10d0
xiezhenglong <xiezhenglong@cestc.cn>
2022-07-16 11:36:32 +0800

    update:优化代码

--- pkg/data_sync/data_sync.go
+++ pkg/data_sync/data_sync.go
@@ -2,7 +2,6 @@ package data_sync
 
 import (
 	"code.cestc.cn/ccos-ops/cloud-monitor/common/logger"
-	"code.cestc.cn/ccos-ops/cloud-monitor/common/util/run_time"
 	"errors"
 	"time"
 )
@@ -10,12 +9,13 @@ import (
 type Task interface {
 	Run() error
 }
+type ClusterTask interface {
+	Run(isMaster func() bool) error
+}
 
 type SyncTask interface {
 	Task
 	Period() time.Duration
-	SetLoop(chan SyncTask)
-	Loop() chan SyncTask
 }
 
 type CombinedSynchronizer struct {
@@ -23,33 +23,45 @@ type CombinedSynchronizer struct {
 	Tasks []SyncTask
 }
 
-func NewCombinedSynchronizer(tasks []SyncTask) (Task, error) {
+func NewCombinedSynchronizer(tasks []SyncTask) (ClusterTask, error) {
 	if len(tasks) == 0 {
 		return nil, errors.New("任务不能为空")
 	}
 	return &CombinedSynchronizer{
 		Tasks: tasks,
-		Loop:  make(chan SyncTask),
+		Loop:  make(chan SyncTask, 10),
 	}, nil
 }
 
-func (cs *CombinedSynchronizer) Run() error {
+func (cs *CombinedSynchronizer) Run(isMaster func() bool) error {
 	go func() {
 		for {
 			select {
 			case st := <-cs.Loop:
-				go run_time.SafeRun(func() {
+				if !isMaster() {
+					goto EXIT
+				}
+				go func(t SyncTask) {
+					defer func() {
+						if e := recover(); e != nil {
+							logger.Logger().Errorf("sync run time error, %v", e)
+						}
+						time.Sleep(st.Period())
+						cs.Loop <- st
+					}()
 					err := st.Run()
 					if err != nil {
 						logger.Logger().Errorf("同步数据：出错原因：%v ", err)
 					}
-				})
+				}(st)
 			}
 		}
+	EXIT:
+		logger.Logger().Info("master killed...")
 	}()
 	for i := range cs.Tasks {
-		cs.Tasks[i].SetLoop(cs.Loop)
 		cs.Loop <- cs.Tasks[i]
 	}
+
 	return nil
 }
--- pkg/data_sync/sync_init/sync_init.go
+++ pkg/data_sync/sync_init/sync_init.go
@@ -2,16 +2,10 @@ package sync_init
 
 import (
 	"code.cestc.cn/ccos-ops/cloud-monitor/common/logger"
-	"code.cestc.cn/ccos-ops/cloud-monitor/common/util/run_time"
-	"code.cestc.cn/ccos-ops/cloud-monitor/pkg/business-common/global/sys_component/sys_redis"
-	"code.cestc.cn/ccos-ops/cloud-monitor/pkg/constant"
 	"code.cestc.cn/ccos-ops/cloud-monitor/pkg/data_sync"
 	"code.cestc.cn/ccos-ops/cloud-monitor/pkg/data_sync/sync_biz"
-	"context"
-	"github.com/go-redis/redis/v8"
-	"log"
+	"github.com/google/uuid"
 	"strings"
-	"time"
 )
 
 func InitSync(regionRole string) error {
@@ -25,99 +19,15 @@ func InitSync(regionRole string) error {
 	}
 	logger.Logger().Info("current run mode: multiple regions - region")
 
-	//同步器心跳检测
-	go run_time.SafeRun(func() {
-		startHeartBeatCheckTask()
-	})
-	return startSyncTask()
-
-}
-
-func startHeartBeatCheckTask() {
-	logger.Logger().Info("start sync heartbeat check ")
-	ticker := time.NewTicker(60 * time.Second)
-	for {
-		select {
-		case <-ticker.C:
-			val, err := sys_redis.Get(constant.SyncFlagKey)
-			if err != nil && err != redis.Nil {
-				logger.Logger().Infof("get started flag error, %v", err)
-			}
-			if len(val) == 0 {
-				logger.Logger().Info("sync task is null, start running sync task")
-				err := startSyncTask()
-				if err != nil {
-					logger.Logger().Info("start sync task error, %v", err)
-				}
-			}
-		}
-	}
-}
-
-func startSyncTask() error {
-	ctx := context.Background()
-	lockKey := constant.SyncStartKey
-	var started = make(chan int)
-
-	defer func() {
-		//休眠1s，确保已成功发送心跳
-		<-started
-		err := sys_redis.Unlock(ctx, lockKey)
-		logger.Logger().Info("release sync lock")
+	err := NewMyExclusiveTask("syncDataTask", uuid.New().String(), func(isMaster func() bool) error {
+		synchronizer, err := data_sync.NewCombinedSynchronizer([]data_sync.SyncTask{sync_biz.NewContactSynchronizer(),
+			sync_biz.NewAlarmRuleSynchronizer(), sync_biz.NewAlarmRecordSynchronizer()})
 		if err != nil {
-			logger.Logger().Errorf("unlock errorL%+v, lock:%s", err, lockKey)
+			return err
 		}
-	}()
+		return synchronizer.Run(isMaster)
+	}).Run()
 
-	err := sys_redis.Lock(ctx, lockKey, sys_redis.DefaultLease, true)
-	if err != nil {
-		log.Printf("获取 rule lock error  %+v", err)
-		return err
-	}
-	logger.Logger().Info("get SyncStartKey lock")
-	val, err := sys_redis.Get(constant.SyncFlagKey)
-	if err != nil && err != redis.Nil {
-		logger.Logger().Errorf("get sync flag key error, %v", err)
-		return nil
-	}
-	if len(val) > 0 {
-		logger.Logger().Info("already start sync task, not need running ")
-		started <- 1
-		return nil
-	}
-	logger.Logger().Info("sync task not running, start running...")
-	synchronizer, err := data_sync.NewCombinedSynchronizer([]data_sync.SyncTask{sync_biz.NewContactSynchronizer(), sync_biz.NewAlarmRuleSynchronizer(), sync_biz.NewAlarmRecordSynchronizer()})
-	if err != nil {
-		return err
-	}
-	err = synchronizer.Run()
-	if err != nil {
-		return err
-	}
-	heartBeatTask()
-	started <- 1
-	logger.Logger().Info("sync task is running success!")
-	return nil
-}
+	return err
 
-func heartBeatTask() {
-	heartBeatFlag := func() {
-		err := sys_redis.SetByTimeOut(constant.SyncFlagKey, "started", 30*time.Second)
-		if err != nil {
-			logger.Logger().Info("set started flag error, %v", err)
-		}
-	}
-	heartBeatFlag()
-	//同步器心跳
-	go run_time.SafeRun(func() {
-		logger.Logger().Info("start sync heartbeat... ")
-		ticker := time.NewTicker(10 * time.Second)
-		defer ticker.Stop()
-		for {
-			select {
-			case <-ticker.C:
-				heartBeatFlag()
-			}
-		}
-	})
 }
