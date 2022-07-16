0a79e5d34c52b51f1a28751c887a546c96bac786
xiezhenglong <xiezhenglong@cestc.cn>
2022-07-16 11:36:32 +0800

    update:优化代码

--- pkg/constant/redis_key.go
+++ pkg/constant/redis_key.go
@@ -1,8 +1,9 @@
 package constant
 
 const (
-	TenantInfoKey = "cec-hawkeye:userInfo:%s"   //缓存租户信息
-	TenantRuleKey = "cec-hawkeye:rule:%s"       //租户获取规则锁key
-	SyncStartKey  = "cec-hawkeye:syncStart"     //同步器启动锁Key
-	SyncFlagKey   = "cec-hawkeye:syncStartFlag" //同步器标记
+	TenantInfoKey = "cec-hawkeye:userInfo:%s" //缓存租户信息
+	TenantRuleKey = "cec-hawkeye:rule:%s"     //租户获取规则锁key
+
+	SyncTaskKey          = "task_%s_started" //同步器MasterId
+	SyncTaskStartLockKey = "task_%s_start_lock"
 )
--- pkg/data_sync/sync_biz/base_biz_sync.go
+++ pkg/data_sync/sync_biz/base_biz_sync.go
@@ -5,7 +5,6 @@ import (
 	"code.cestc.cn/ccos-ops/cloud-monitor/pkg/business-common/global"
 	"code.cestc.cn/ccos-ops/cloud-monitor/pkg/business-common/model"
 	"code.cestc.cn/ccos-ops/cloud-monitor/pkg/data_sync"
-	"time"
 )
 
 const (
@@ -22,29 +21,9 @@ type SyncDataTask interface {
 type BaseSyncTask struct {
 	Task    SyncDataTask
 	BizCode string
-	loop    chan data_sync.SyncTask
-}
-
-func (bst *BaseSyncTask) Period() time.Duration {
-	return bst.Task.Period()
-}
-func (bst *BaseSyncTask) SetLoop(loop chan data_sync.SyncTask) {
-	bst.loop = loop
-}
-func (bst *BaseSyncTask) Loop() chan data_sync.SyncTask {
-	return bst.loop
-}
-
-func (bst *BaseSyncTask) Work(time string) (string, error) {
-	currentTime, err := bst.Task.Work(time)
-	return currentTime, err
 }
 
 func (bst *BaseSyncTask) Run() error {
-	defer func() {
-		time.Sleep(bst.Period())
-		bst.Loop() <- bst.Task
-	}()
 	lastUpdateTime, err := bst.getUpdateTime()
 	if err != nil {
 		return err
@@ -53,11 +32,7 @@ func (bst *BaseSyncTask) Run() error {
 	if err != nil {
 		return err
 	}
-	err = bst.updateTime(currentTime)
-	if err != nil {
-		return err
-	}
-	return nil
+	return bst.updateTime(currentTime)
 }
 
 func (bst *BaseSyncTask) getUpdateTime() (string, error) {
