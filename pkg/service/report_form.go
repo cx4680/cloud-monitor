3b30eab8245576b3e323bb195ea265d344fb13ad
ningminglong <ningminglong@cestc.cn>
2023-07-28 17:20:14 +0800

    dns导出没数据

--- pkg/service/report_form.go
+++ pkg/service/report_form.go
@@ -42,7 +42,8 @@ func (s *ReportFormService) GetMonitorData(param form.ReportFormParam) ([]*form.
 	instances := strings.Join(instanceList, "|")
 	item := dao.MonitorItem.GetMonitorItemCacheByName(param.ItemList[0])
 	labels := strings.Split(item.Labels, ",")
-
+	fmt.Println(fmt.Printf("导出dns：%v,%v", item, instances))
+	fmt.Println(fmt.Printf("param：%v", param))
 	pql := strings.ReplaceAll(item.MetricsLinux, constant.MetricLabel, constant.INSTANCE+"=~'"+instances+"'")
 	if item.Name == "private_dns_dns_requests_total" || item.MetricName == "private_dns_dns_requests_total_rate1m" {
 		pql = strings.ReplaceAll(item.MetricsLinux, constant.MetricLabel, "instanceId=~'"+instances+"'")
